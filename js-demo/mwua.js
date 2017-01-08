0f982aaca8fd6008ea80891c3a36719a3f8441ae
Jeremy Kun <kun.jeremy@gmail.com>
2017-01-08 09:25:14 +0100

    js-demo: add initial mwua routine

--- js-demo/mwua.js
+++ js-demo/mwua.js
@@ -11,7 +11,7 @@ function initializeWeights(n) {
   return weights;
 }
 
-function sample(weights) {
+function draw(weights) {
   // Sample an index proportionally to the size of the number at that index
   var sum = 0;
   for (var i = 0; i < weights.length; i++) {
@@ -30,4 +30,22 @@ function sample(weights) {
 
     choiceIndex += 1;
   }
+}
+
+function mwua(objects, observeOutcome, reward, learningRate, numRounds) {
+  var weights = initializeWeights(objects.length);
+  
+  for (var i = 0; i < numRounds; i++) {
+    chosenObjectIndex = draw(weights);
+    chosenObject = objects[chosenObjectIndex];
+
+    outcome = observeOutcome(t, chosenObject);
+    thisRoundReward = reward(chosenObject, outcome);
+
+    for (var j = 0; j < weights.length; j++) {
+      weights[j] *= (1 + learningRate * reward(objects[j], outcome))
+    }
+  }
+
+  return weights;
 }
