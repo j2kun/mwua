4a95f93e321de65350928c2d36c880540144b7d7
Jeremy Kun <kun.jeremy@gmail.com>
2017-02-14 08:25:46 -0800

    js-demo: add main mwua routine

--- js-demo/build/main.bundle.js
+++ js-demo/build/main.bundle.js
@@ -1,18 +1,103 @@
 'use strict';
 
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
-    var monthlyRate = 0;
-    if (rate) {
-        monthlyRate = rate / 100 / 12;
-    }
-    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-    return monthlyPayment;
+var state = {
+    objects: [1, 2, 3, 4],
+    weights: [1, 1, 1, 1],
+    learningRate: 0.5,
+    roundRewards: [],
+    cumulativeRewards: [0, 0, 0, 0]
 };
 
+function sum(arr) {
+    return arr.reduce(function (sum, x) {
+        return sum + x;
+    });
+}
+
+// Choose an index of the weights array at random, proportionally to the value
+// at that index.
+function draw(weights) {
+    var choice = Math.random() * sum(weights);
+    var choiceIndex = 0;
+
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = weights[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var weight = _step.value;
+
+            choice = choice - weight;
+            if (choice <= 0) {
+                return choiceIndex;
+            }
+
+            choiceIndex += 1;
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    throw { 'error': 'How did I get here? Is this real life?' };
+}
+
+function selectObject(objects, weights) {
+    return objects[draw(weights)];
+}
+
+function mwuaStep(objects, weights, chosenObject, chosenRewards, learningRate) {
+    var chosenObjectIndex = objects.indexOf(chosenObject);
+    for (var i = 0; i < weights.length; i++) {
+        weights[i] *= 1 + learningRate * chosenRewards[i];
+    }
+}
+
+function executeRound(chosenObject, chosenRewards) {
+    mwuaStep(state.objects, state.weights, chosenObjects, chosenRewards, state.learningRate);
+    state.roundRewards.push(chosenRewards);
+    for (var i = 0; i < chosenRewards.length; i++) {
+        state.cumulativeRewards[i] += chosenRewards[i];
+    }
+
+    updateUI(chosenRewards);
+}
+
+function updateUI(chosenRewards) {
+    var row = chosenRewards.map(function (n) {
+        return '<td>' + n.toString() + '</td>';
+    }).join('');
+    // TODO: remove chosenRewards class from last row, remove input fields and replace with text of chosenRewards
+    $('#rewards tr:last').after('<tr>' + row + '</tr>');
+    // TODO: add new input row to rewards table
+    /*
+        <th id="round0">Round 1: </th>
+            <td><input>1</input></td>
+            <td><input>1</input></td>
+            <td><input>1</input></td>
+            <td><input>1</input></td>
+        </tr>
+    */
+    // TODO: update cumulativeRewards row
+    // TODO: update chosenObject span and mwuaBest span
+}
+
+/*
 document.getElementById('calcBtn').addEventListener('click', function () {
-    var principal = document.getElementById("principal").value;
-    var years = document.getElementById("years").value;
-    var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
-    document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+   return;
 });
+*/
+
+// selectObject(state.objects, state.weights)
+// mwuaStep(state.objects, state.weights, 2, [2,3,4,5], state.learningRate)
--- js-demo/js/main.js
+++ js-demo/js/main.js
@@ -1,7 +1,76 @@
-function mwua() {
+var state = {
+    objects: [1, 2, 3, 4],
+    weights: [1, 1, 1, 1],
+    learningRate: 0.5,
+    roundRewards: [],
+    cumulativeRewards: [0, 0, 0, 0]
+};
 
+function sum(arr) {
+    return arr.reduce((sum, x) => sum + x);
 }
 
+// Choose an index of the weights array at random, proportionally to the value
+// at that index.
+function draw(weights) {
+    let choice = Math.random() * sum(weights);
+    let choiceIndex = 0;
+
+    for (let weight of weights) {
+        choice = choice - weight;
+        if (choice <= 0) {
+            return choiceIndex;
+        }
+
+        choiceIndex += 1;
+    }
+
+    throw {'error': 'How did I get here? Is this real life?'};
+}
+
+function selectObject(objects, weights) {
+    return objects[draw(weights)];
+}
+
+function mwuaStep(objects, weights, chosenObject, chosenRewards, learningRate) {
+    let chosenObjectIndex = objects.indexOf(chosenObject);
+    for (let i=0; i < weights.length; i++) {
+        weights[i] *= (1 + learningRate * chosenRewards[i])
+    }
+}
+
+function executeRound(chosenObject, chosenRewards) {
+    mwuaStep(state.objects, state.weights, chosenObjects, chosenRewards, state.learningRate);
+    state.roundRewards.push(chosenRewards);
+    for (let i=0; i < chosenRewards.length; i++) {
+        state.cumulativeRewards[i] += chosenRewards[i];
+    }
+
+    updateUI(chosenRewards);
+}
+
+function updateUI(chosenRewards) {
+    let row = chosenRewards.map(n => '<td>' + n.toString() + '</td>').join('')
+    // TODO: remove chosenRewards class from last row, remove input fields and replace with text of chosenRewards
+    $('#rewards tr:last').after('<tr>' + row + '</tr>');
+    // TODO: add new input row to rewards table
+    /*
+        <th id="round0">Round 1: </th>
+            <td><input>1</input></td>
+            <td><input>1</input></td>
+            <td><input>1</input></td>
+            <td><input>1</input></td>
+        </tr>
+    */
+    // TODO: update cumulativeRewards row
+    // TODO: update chosenObject span and mwuaBest span
+}
+
+/*
 document.getElementById('calcBtn').addEventListener('click', function () {
    return;
 });
+*/
+
+// selectObject(state.objects, state.weights)
+// mwuaStep(state.objects, state.weights, 2, [2,3,4,5], state.learningRate)
