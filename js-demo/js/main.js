0e75f2117a272620ffa17de5d064cf9147166b69
Jeremy Kun <kun.jeremy@gmail.com>
2017-02-18 18:16:40 -0800

    js: add render loop

--- js-demo/js/main.js
+++ js-demo/js/main.js
@@ -1,10 +1,4 @@
-var state = {
-    objects: [1, 2, 3, 4],
-    weights: [1, 1, 1, 1],
-    learningRate: 0.5,
-    roundRewards: [],
-    cumulativeRewards: [0, 0, 0, 0]
-};
+import $ from 'jquery';
 
 function sum(arr) {
     return arr.reduce((sum, x) => sum + x);
@@ -39,38 +33,66 @@ function mwuaStep(objects, weights, chosenObject, chosenRewards, learningRate) {
     }
 }
 
-function executeRound(chosenObject, chosenRewards) {
-    mwuaStep(state.objects, state.weights, chosenObjects, chosenRewards, state.learningRate);
+
+// Initialize state
+global.state = {
+    objects: ['Alice', 'Bob', 'Chadwick', 'Eve'],
+    weights: [1, 1, 1, 1],
+    learningRate: 0.5,
+    roundRewards: [],
+    cumulativeRewards: [0, 0, 0, 0]
+};
+state.chosenObject = selectObject(state.objects, state.weights);
+
+
+function executeRound(chosenRewards) {
+    mwuaStep(state.objects, state.weights, state.chosenObject, chosenRewards, state.learningRate);
     state.roundRewards.push(chosenRewards);
     for (let i=0; i < chosenRewards.length; i++) {
         state.cumulativeRewards[i] += chosenRewards[i];
     }
-
-    updateUI(chosenRewards);
 }
 
-function updateUI(chosenRewards) {
-    let row = chosenRewards.map(n => '<td>' + n.toString() + '</td>').join('')
-    // TODO: remove chosenRewards class from last row, remove input fields and replace with text of chosenRewards
-    $('#rewards tr:last').after('<tr>' + row + '</tr>');
-    // TODO: add new input row to rewards table
-    /*
-        <th id="round0">Round 1: </th>
-            <td><input>1</input></td>
-            <td><input>1</input></td>
-            <td><input>1</input></td>
-            <td><input>1</input></td>
-        </tr>
-    */
-    // TODO: update cumulativeRewards row
-    // TODO: update chosenObject span and mwuaBest span
+function renderRewardRow(rewards, index) {
+    console.log(rewards);
+    console.log(index);
+    let row = rewards.map(n => '<td>' + n.toString() + '</td>').join('');
+    return '<tr>' + 
+           '<th> Round <span class="roundNumber">' + (index + 1) + '</span>:</th>' + 
+           row + 
+           '</tr>'
 }
 
-/*
-document.getElementById('calcBtn').addEventListener('click', function () {
-   return;
+function renderRewardTable(roundRewards) {
+    var rewardRows = roundRewards.map(renderRewardRow).join("\n");
+    var headerRow = "<tr class='header'> <th>Experts:</th> <td>Alice</td> <td>Bob</td>" + 
+                    " <td>Chadwick</td> <td>Eve</td> </tr>";
+    var inputRow = "<tr id=\"inputRewards\"> <th>Round <span class=\"roundNumber\">" + 
+                   (roundRewards.length + 1) + 
+                   "</span>: </th> <td><input></td> <td><input></td> <td><input></td> <td><input></td> </tr>"
+
+    return [headerRow, rewardRows, inputRow].join('\n');
+}
+
+export function render(state) {
+    $('#rewards').html(renderRewardTable(state.roundRewards));
+    $('input').val('');
+    $('#cumulativeRewards td').each(function(index) {
+        $( this ).text(state.cumulativeRewards[index]);
+    });
+    $('h2 .roundNumber').text(state.roundRewards.length);
+    $('#chosenObject').text(state.chosenObject);
+    // TODO: set MWUA weights
+}
+
+$('#calcBtn').click(function () {
+    var chosenRewards = $.makeArray($('#inputRewards input').map(function() {
+        return parseFloat($( this ).val()) || 0;
+    }));
+    console.log(chosenRewards);
+    executeRound(chosenRewards);
+    state.chosenObject = selectObject(state.objects, state.weights);
+
+    render(state);
 });
-*/
 
-// selectObject(state.objects, state.weights)
-// mwuaStep(state.objects, state.weights, 2, [2,3,4,5], state.learningRate)
